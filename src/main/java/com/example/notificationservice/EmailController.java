0ea5dc7a8dcbb700d2e2a6e11d547f9f4baebb99
Quang Duong <quangduong19992001@gmail.com>
2024-11-17 02:16:49 +0700

    .
    .

--- src/main/java/com/example/notificationservice/EmailController.java
+++ src/main/java/com/example/notificationservice/EmailController.java
@@ -3,6 +3,8 @@ package com.example.notificationservice;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import java.io.IOException;
 @RequestMapping("/mail-api")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = lombok.AccessLevel.PRIVATE)
+@Slf4j
 public class EmailController {
 
     JavaMailSender mailSender;
@@ -24,4 +27,10 @@ public class EmailController {
     public void sendEmail() throws MessagingException, IOException {
         emailService.sendHtmlEmail("Quang Duong", "ct040213@actvn.edu.vn", "https://example.com/verify?token=abc123");
     }
+
+    @KafkaListener(topics = "send-email", groupId = "notification-service")
+    public void consume(String message) throws MessagingException, IOException {
+        log.info("message: {}", message);
+    }
+
 }
